4e944f0fdd0a095feabe459954d66ad829680709
Vitor Cruz <vitor.dasilvacruz@gmail.com>
2020-01-07 19:40:48 -0300

    new portfolio design

---
+++ gallery.85dd0424.js
@@ -0,0 +1,21 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"QdKD":[function(require,module,exports) {
+module.exports="/1.cf445ece.png";
+},{}],"NPU7":[function(require,module,exports) {
+module.exports="/2.12ee94ab.png";
+},{}],"jfTC":[function(require,module,exports) {
+module.exports="/3.f7ef4e6e.png";
+},{}],"VqNJ":[function(require,module,exports) {
+module.exports="/4.9a489e9a.png";
+},{}],"Pabc":[function(require,module,exports) {
+module.exports="/gallery_magnify.6ae14448.png";
+},{}],"Bj0o":[function(require,module,exports) {
+module.exports="/react-bounds.js.0ffa35a6.png";
+},{}],"lCVE":[function(require,module,exports) {
+module.exports="/travel_page1.3e76698b.png";
+},{}],"fNK2":[function(require,module,exports) {
+module.exports="/travel_page.284a78f2.png";
+},{}],"MFSP":[function(require,module,exports) {
+module.exports="/web_page_flutter.c292a0ca.png";
+},{}],"U9vj":[function(require,module,exports) {
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=void 0;var e=i(require("../assets/tecprodbox/1.png")),t=i(require("../assets/tecprodbox/2.png")),o=i(require("../assets/tecprodbox/3.png")),r=i(require("../assets/tecprodbox/4.png")),p=i(require("../assets/gallery_magnify.png")),a=i(require("../assets/react-bounds.js.png")),s=i(require("../assets/travel_page1.png")),d=i(require("../assets/travel_page.png")),n=i(require("../assets/web_page_flutter.png"));function i(e){return e&&e.__esModule?e:{default:e}}var u=[{photo:e.default,project:"TecProdBox",description:"Front End home page"},{photo:t.default,project:"TecProdBox",description:"Front End box page"},{photo:o.default,project:"TecProdBox",description:"Mobile app home page"},{photo:r.default,project:"TecProdBox",description:"Mobile app box page"},{photo:p.default,project:"HTML_TEMPLATES",description:"Front end Gallery with magnify"},{photo:a.default,project:"react-bounds.js",description:"Front end page"},{photo:s.default,full:d.default,project:"TRAVEL_PAGE",description:"Front end of Travel page template"},{photo:n.default,project:"Flutter web page",description:"Front end web page writed with flutter"}];exports.default=u;
+},{"../assets/tecprodbox/1.png":"QdKD","../assets/tecprodbox/2.png":"NPU7","../assets/tecprodbox/3.png":"jfTC","../assets/tecprodbox/4.png":"VqNJ","../assets/gallery_magnify.png":"Pabc","../assets/react-bounds.js.png":"Bj0o","../assets/travel_page1.png":"lCVE","../assets/travel_page.png":"fNK2","../assets/web_page_flutter.png":"MFSP"}]},{},["U9vj"], null)
